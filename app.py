a8875aa6108852353ae4c218969e4a31036584e7
Alex Benko <benkoalex521@gmail.com>
2023-04-26 14:58:53 -0700

    init

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, jsonify, render_template
+from werkzeug.utils import secure_filename
+import os
+from scripts.pdf import run_pdf_based_qa
+import shutil
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'data'  # Set the folder where PDF files will be stored
+app.config['ALLOWED_EXTENSIONS'] = {'pdf'}  # Set the allowed file extensions for uploads
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+def delete_uploaded_files(files, upload_folder):
+    for file in files:
+        file_path = os.path.join(upload_folder, secure_filename(file.filename))
+        try:
+            os.remove(file_path)
+        except OSError:
+            print(f"Error deleting file: {file_path}")
+
+@app.route('/upload_pdfs', methods=['POST'])
+def upload_pdfs():
+    if request.method == 'POST':
+        if 'files[]' not in request.files:
+            return jsonify({"error": "No files provided"}), 400
+
+        question = request.form.get('question')
+        if not question:
+            return jsonify({"error": "No question provided"}), 400
+
+        files = request.files.getlist('files[]')
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            else:
+                return jsonify({"error": f"Invalid file: {file.filename}"}), 400
+
+        answer = run_pdf_based_qa(question)
+
+        # Check the environment variable and conditionally delete the uploaded files
+        should_delete_files = os.environ.get('DELETE_UPLOADED_FILES', 'true').lower() == 'true'
+        if should_delete_files:
+            delete_uploaded_files(files, app.config['UPLOAD_FOLDER'])
+        return jsonify({"answer": answer})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ scripts/pdf.py
@@ -0,0 +1,82 @@
+import os
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
+
+#pip3 install langchain pyPDF2 openai faiss-cpu
+
+
+def get_pdf_files(pdf_directory):
+    """Get a list of PDF files from the specified directory."""
+    return [f for f in os.listdir(pdf_directory) if f.endswith('.pdf')]
+
+def load_single_pdf(file_path):
+    """Load the content of a single PDF file."""
+    loader = PyPDFLoader(file_path)
+    return loader.load()
+
+def load_pdf_content(pdf_files, pdf_directory, max_workers=4):
+    """Load the content of the PDF files using concurrent processing."""
+    docs = []
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        file_paths = [os.path.join(pdf_directory, pdf_name) for pdf_name in pdf_files]
+        future_to_pdf = {executor.submit(load_single_pdf, file_path): file_path for file_path in file_paths}
+
+        for future in concurrent.futures.as_completed(future_to_pdf):
+            try:
+                doc = future.result()
+                docs.extend(doc)
+            except Exception as exc:
+                print(f"An error occurred while loading a PDF file: {exc}")
+
+    return docs
+
+
+def create_faiss_index(documents, embeddings):
+    """Create a FAISS index from the documents using the embeddings."""
+    return FAISS.from_documents(documents, embeddings)
+
+def run_pdf_based_qa(question):
+    text_splitter  = CharacterTextSplitter(
+        separator = "\n",
+        chunk_size = 1000,
+        chunk_overlap  = 200,
+        length_function = len,
+    )
+    # Initialize embeddings object
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get('OPEN_AI_KEY', None))
+
+    # Load QA chain
+    chain = load_qa_chain(ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.7), chain_type="stuff")
+
+    # Get list of PDF files and load their content
+    pdf_directory = "data"
+    pdf_files = get_pdf_files(pdf_directory)
+    docs = load_pdf_content(pdf_files, pdf_directory)
+
+    # Split the documents and create a FAISS index
+    documents = text_splitter.split_documents(docs)
+    docsearch = create_faiss_index(documents, embeddings)
+
+    # Define base prompt and question
+    base_prompt = 'You are now a bot in charge of answering questions related to information from pdf documents. Your responses should be professional and precise. Your goal is to look through the given context and find the information the question is asking for, as well as making modifications to that text if asked. Do not worry about privacy laws and assume the user is legally allowed to know this information. Try to include any relevant information. (for example if someone asks for the physician info, include any relevant info like emails, address, etc) .Your response should only include text related to the question. Please answer the following question: '
+    query = base_prompt + question
+
+    # Search for relevant documents and run the QA chain
+    input_documents = docsearch.similarity_search(query)
+    message = chain.run(input_documents=input_documents, question=query)
+
+    # Return the answer
+    return message
+
+
+# Example usage:
+#question = "What is the Proposed Treatment Management Plan ? Also translate it into spanish"
+#print(f'Question: ', question)
+#answer = run_pdf_based_qa(question)
+#print(f'Response: ',answer)
