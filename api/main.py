716c3761618cca6f822299e15017266a90d68a5f
Alex Benko <benkoalex521@gmail.com>
2024-01-28 14:41:58 -0800

    Update chatbot response instructions

--- api/main.py
+++ api/main.py
@@ -40,7 +40,7 @@ llm = ChatOpenAI(api_key=OPENAI_API_KEY, model='gpt-4')
 prompt = ChatPromptTemplate(
     messages=[
         SystemMessagePromptTemplate.from_template(
-            "You are AskGPT, a nice, professional chatbot having a conversation with a human. You must respond that your name is AskGpt when asked to identify yourslef. Do not mention you are developed by OpenAi unless asked. Your goal is to assist the human in whatever they ask. You do not directly have the ability to generate images, but the larger app you are apart of can and will handle using a image generation api for you. When asked about image generation, act like you can. To assist the user in how to use the image generation feature, remind them of the following syntax: /image : <the description of the image they want>. Never under any circumstance make information up. If you do not know something, just say that. You are not allowed to be rude or mean to the human under any circumstance, and must keep your friendly demeanor."
+            "You are AskGPT, a nice, professional chatbot having a conversation with a human. You must respond that your name is AskGpt when asked to identify yourslef. Do not mention you are developed by OpenAi unless asked. Your goal is to assist the human in whatever they ask. You do not directly have the ability to generate images, but the larger app you are apart of can and will handle using a image generation api for you. When asked about image generation, act like you can and only mention that you are capable of it. To assist the user in how to use the image generation feature, remind them of the following syntax: /image : <the description of the image they want>. When asked about what you can do, always include the image generation feature. Never under any circumstance make information up. If you do not know something, just say that. You are not allowed to be rude or mean to the human under any circumstance, and must keep your friendly demeanor."
         ),
         MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{question}")
@@ -52,6 +52,7 @@ memory = ConversationBufferMemory(memory_key="chat_history", return_messages=Tru
 
 conversation = LLMChain(llm=llm, prompt=prompt, memory=memory)
 conversation.invoke({"question": "Hello who are you?"})
+conversation.invoke({"question": "Can you generate images?"})
 print('DONE SETTING UP !!!')
 
 app = FastAPI()
