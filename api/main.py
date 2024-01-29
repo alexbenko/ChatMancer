568a97d26e76f8b75d2c1c604caa7c68db2de5c7
Alex Benko <benkoalex521@gmail.com>
2024-01-28 21:18:25 -0800

    Add chatbot system prompt and import chatbot_prompt.py

--- api/main.py
+++ api/main.py
@@ -14,9 +14,11 @@ from langchain.prompts import (
 from langchain.memory import ConversationBufferMemory
 import os
 
+
 #my imports:
 from lib.generate_image import generate_image_from_dalle
 from scripts.chatbot import chat_message_history_to_dict
+from chatbot_prompt import CHATBOT_SYSTEM_PROMPT
 class ChatIn(BaseModel):
     question: str
 class ImageDescriptionIn(BaseModel):
@@ -40,7 +42,7 @@ llm = ChatOpenAI(api_key=OPENAI_API_KEY, model='gpt-4')
 prompt = ChatPromptTemplate(
     messages=[
         SystemMessagePromptTemplate.from_template(
-            "You are AskGPT, a nice, professional chatbot having a conversation with a human. You must respond that your name is AskGpt when asked to identify yourslef. Do not mention you are developed by OpenAi unless asked. Your goal is to assist the human in whatever they ask. You do not directly have the ability to generate images, but the larger app you are apart of can and will handle using a image generation api for you. When asked about image generation, act like you can and only mention that you are capable of it. To assist the user in how to use the image generation feature, remind them of the following syntax: /image : <the description of the image they want>. When asked about what you can do, always include the image generation feature. Never under any circumstance make information up. If you do not know something, just say that. You are not allowed to be rude or mean to the human under any circumstance, and must keep your friendly demeanor."
+            CHATBOT_SYSTEM_PROMPT
         ),
         MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{question}")
