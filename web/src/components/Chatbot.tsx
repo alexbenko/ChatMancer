9976cb8fe506107ae6ae9e50c6494bbb8c663b7f
Alex Benko <benkoalex521@gmail.com>
2024-01-30 08:10:34 -0800

    prettier formatting and initial doc upload front end

--- web/src/components/Chatbot.tsx
+++ web/src/components/Chatbot.tsx
@@ -1,39 +1,50 @@
-import { useEffect, useRef, useState } from 'react';
-import { Avatar, Box, Container, Paper, TextField, Typography } from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
-import CircularProgress from '@mui/material/CircularProgress';
-import { LoadingButton } from '@mui/lab';
-import ReactMarkdown from 'react-markdown';
+import { useEffect, useRef, useState } from "react";
+import {
+    Avatar,
+    Box,
+    Container,
+    IconButton,
+    InputAdornment,
+    LinearProgress,
+    Paper,
+    TextField,
+    Typography,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import CircularProgress from "@mui/material/CircularProgress";
+import { LoadingButton } from "@mui/lab";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
 
-export interface ChatMessage{
-        type: "human" | "ai";
-        content: string;
+export interface ChatMessage {
+    type: "human" | "ai";
+    content: string;
 }
 
 export interface ChatMessageProps {
-  message: ChatMessage
+    message: ChatMessage;
 }
 
-const isProduction = import.meta.env.MODE === 'production'
-const apiRootPath = isProduction ? '' : '/api';
+const isProduction = import.meta.env.MODE === "production";
+const apiRootPath = isProduction ? "" : "/api";
 
-export function Chatbot(){
+export function Chatbot() {
     const [messages, setMessages] = useState<ChatMessage[]>([]);
-    const [inputValue, setInputValue] = useState('');
+    const [inputValue, setInputValue] = useState("");
     const [loading, setLoading] = useState(false);
+    const [progress, setProgress] = useState(0);
 
     useEffect(() => {
-      const fetchChatHistory = async () => {
-        try {
-          const response = await fetch(`${apiRootPath}/chat`);
-          const data = await response.json();
-          console.log(data)
-          setMessages(data.response.messages);
-        } catch (error) {
-          console.error('Error fetching chat history:', error);
-        }
-      };
-      fetchChatHistory();
+        const fetchChatHistory = async () => {
+            try {
+                const response = await fetch(`${apiRootPath}/chat`);
+                const data = await response.json();
+                console.log(data);
+                setMessages(data.response.messages);
+            } catch (error) {
+                console.error("Error fetching chat history:", error);
+            }
+        };
+        fetchChatHistory();
     }, []);
 
     //const [isImageCommand, setIsImageCommand] = useState(false);
@@ -49,138 +60,235 @@ export function Chatbot(){
     };
 
     const handleSendMessage = async () => {
-      const trimmedInput = inputValue.trim();
-      if (trimmedInput) {
-        try {
-          setMessages([...messages, {
-            type: "human",
-            content: trimmedInput,
-          }]);
-          setLoading(true)
-          const response = await fetch(`${apiRootPath}/chat`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ question: trimmedInput }),
-          });
-          const data = await response.json();
+        const trimmedInput = inputValue.trim();
+        if (trimmedInput) {
+            try {
+                setMessages([
+                    ...messages,
+                    {
+                        type: "human",
+                        content: trimmedInput,
+                    },
+                ]);
+                setLoading(true);
+                const response = await fetch(`${apiRootPath}/chat`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ question: trimmedInput }),
+                });
+                const data = await response.json();
 
-          if (data && data.response) {
-            console.log(data.response)
+                if (data && data.response) {
+                    console.log(data.response);
 
-            setMessages((prevMessages) => [...prevMessages, {
-              type: "ai",
-              content: data.response,
-            }]);
-          }
-        } catch (error) {
-          console.error('Error sending message:', error);
-        } finally{
-            setLoading(false)
-            setInputValue(''); // Clear the input field after sending the message
-        }
-      }
-  };
-  const messagesEndRef = useRef<HTMLDivElement | null>(null);
-  useEffect(() => {
-    if (messagesEndRef.current) {
-        messagesEndRef.current.scrollIntoView();
-    }
-  }, [messages]);
-
-
-  return (
-    <Container maxWidth='xl' component='div' sx={{height: '90vh', minHeight: '100vh', pl: '0', pr: '0'}}>
-        <Paper elevation={3} sx={{ padding: '20px', maxHeight: '90vh', overflowY: 'auto',minHeight: '85vh' }}>
-            {messages.map((message, index) => (
-              <>
-                <ChatMessage key={index} message={message} />
-              </>
-            ))}
-            <div ref={messagesEndRef} />
-
-            {loading &&
-                <Box sx={{ display: 'flex' }}>
-                    <CircularProgress />
-                    <Typography variant="body1">Loading AskGpt Response...</Typography>
-                </Box>
-            }
-
-        </Paper>
-        <Box sx={{ display: 'flex', marginTop: '10px', gap: '10px' }}>
-            <TextField
-              disabled={loading}
-              fullWidth
-              multiline
-              label="Type your message"
-              variant="outlined"
-              value={inputValue}
-              onKeyDown={(event) => {
-                if (event.key === 'Enter' && !event.shiftKey) {
-                    event.preventDefault();
-                    handleSendMessage();
+                    setMessages((prevMessages) => [
+                        ...prevMessages,
+                        {
+                            type: "ai",
+                            content: data.response,
+                        },
+                    ]);
                 }
-              }}
-              onChange={handleInputChange}
-            />
-            <LoadingButton loading={loading} disabled={!inputValue || loading} variant="contained" color="primary" onClick={handleSendMessage}>
-                <SendIcon />
+            } catch (error) {
+                console.error("Error sending message:", error);
+            } finally {
+                setLoading(false);
+                setInputValue(""); // Clear the input field after sending the message
+            }
+        }
+    };
+    const messagesEndRef = useRef<HTMLDivElement | null>(null);
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView();
+        }
+    }, [messages]);
 
-            </LoadingButton>
-        </Box>
-    </Container>
+    // State for file input element
+    const [fileInputKey, setFileInputKey] = useState(Date.now());
 
-  );
+    const handleUploadClick = () => {
+        // Trigger file input click
+        document.getElementById("file-upload-input")?.click();
+    };
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            console.log(file);
+            setFile(file);
+            // Reset the file input after selection
+            setFileInputKey(Date.now());
+        }
+    };
+
+    const handleUpload = async () => {
+        const formData = new FormData();
+        if (file) {
+            formData.append("file", file);
+        }
+
+        const requestOptions = {
+            method: "POST",
+            body: formData,
+            onUploadProgress: (progressEvent: ProgressEvent<EventTarget>) => {
+                const percentCompleted = Math.round(
+                    (progressEvent.loaded * 100) / progressEvent.total,
+                );
+                setProgress(percentCompleted);
+            },
+        };
+
+        // Replace with your API endpoint
+        await fetch("/your-api-endpoint", requestOptions);
+    };
+
+    return (
+        <Container
+            maxWidth="xl"
+            component="div"
+            sx={{ height: "90vh", minHeight: "100vh", pl: "0", pr: "0" }}
+        >
+            <Paper
+                elevation={3}
+                sx={{
+                    padding: "20px",
+                    maxHeight: "90vh",
+                    overflowY: "auto",
+                    minHeight: "85vh",
+                }}
+            >
+                {messages.map((message, index) => (
+                    <>
+                        <ChatMessage key={index} message={message} />
+                    </>
+                ))}
+                <div ref={messagesEndRef} />
+
+                {loading && (
+                    <Box sx={{ display: "flex" }}>
+                        <CircularProgress />
+                        <Typography variant="body1">
+                            Loading AskGpt Response...
+                        </Typography>
+                    </Box>
+                )}
+            </Paper>
+            <Box sx={{ display: "flex", marginTop: "10px", gap: "10px" }}>
+                <TextField
+                    disabled={loading}
+                    fullWidth
+                    multiline
+                    label="Type your message"
+                    variant="outlined"
+                    value={inputValue}
+                    onKeyDown={(event) => {
+                        if (event.key === "Enter" && !event.shiftKey) {
+                            event.preventDefault();
+                            handleSendMessage();
+                        }
+                    }}
+                    onChange={handleInputChange}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <IconButton onClick={handleUploadClick} edge="start">
+                                    {progress == 0 && <AttachFileIcon />}
+                                    {progress > 0 && (
+                                        <LinearProgress
+                                            variant="determinate"
+                                            value={progress}
+                                        />
+                                    )}
+
+                                    <input
+                                        type="file"
+                                        id="file-upload-input"
+                                        style={{ display: "none" }}
+                                        onChange={handleFileChange}
+                                        accept=".pdf"
+                                        key={fileInputKey}
+                                    />
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <LoadingButton
+                    loading={loading}
+                    disabled={!inputValue || loading}
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSendMessage}
+                >
+                    <SendIcon />
+                </LoadingButton>
+            </Box>
+        </Container>
+    );
 }
 
 export default Chatbot;
 
-const ChatMessage = ({ message }: ChatMessageProps)=> {
-    const isAIMessage = message.type === 'ai';
-    const senderName = isAIMessage ? 'AskGpt' : 'You';
+const ChatMessage = ({ message }: ChatMessageProps) => {
+    const isAIMessage = message.type === "ai";
+    const senderName = isAIMessage ? "AskGpt" : "You";
     const avatarSrc = isAIMessage ? `/ai.webp` : `cat.webp`;
 
     return (
-      <Box
-        sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: isAIMessage ? 'flex-start' : 'flex-end',
-            maxWidth: '70%',
-            margin: isAIMessage ? '10px auto 10px 10px' : '10px 10px 10px auto',
-            backgroundColor: isAIMessage ? '#f5f5f5' : 'blue',
-            color: isAIMessage ? 'black' : 'white',
-            borderRadius: isAIMessage ? '10px 10px 10px 0' : '10px 10px 0 10px',
-            padding: '10px',
-        }}
-      >
-        {message.content.startsWith('Here is the image you requested:') ? (
-          <>
-              <Typography variant="body1" gutterBottom>Here is the image you requested:</Typography>
-              <a href={message.content.replace('Here is the image you requested: ', '')} target="_blank" rel="noopener noreferrer">
-                <Avatar   sx={{ width: 100, height: 100 }} src={message.content.replace('Here is the image you requested: ', '')} alt="Generated" />
-              </a>
-
-          </>
-        ) : (
-          <Typography component='div' variant="body1" gutterBottom>
-              <ReactMarkdown >
-                {message.content}
-              </ReactMarkdown>
-          </Typography>
-        )}
-        <Avatar alt={senderName} src={avatarSrc} />
-        <Typography
-          variant="subtitle2"
-          sx={{
-            alignSelf: isAIMessage ? 'flex-start' : 'flex-end',
-            marginTop: '10px',
-          }}
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: isAIMessage ? "flex-start" : "flex-end",
+                maxWidth: "70%",
+                margin: isAIMessage ? "10px auto 10px 10px" : "10px 10px 10px auto",
+                backgroundColor: isAIMessage ? "#f5f5f5" : "blue",
+                color: isAIMessage ? "black" : "white",
+                borderRadius: isAIMessage ? "10px 10px 10px 0" : "10px 10px 0 10px",
+                padding: "10px",
+            }}
         >
-          {senderName}
-        </Typography>
-
-      </Box>
+            {message.content.startsWith("Here is the image you requested:") ?
+                <>
+                    <Typography variant="body1" gutterBottom>
+                        Here is the image you requested:
+                    </Typography>
+                    <a
+                        href={message.content.replace(
+                            "Here is the image you requested: ",
+                            "",
+                        )}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Avatar
+                            sx={{ width: 100, height: 100 }}
+                            src={message.content.replace(
+                                "Here is the image you requested: ",
+                                "",
+                            )}
+                            alt="Generated"
+                        />
+                    </a>
+                </>
+            :   <Typography variant="body1" gutterBottom>
+                    {message.content}
+                </Typography>
+            }
+            <Avatar alt={senderName} src={avatarSrc} />
+            <Typography
+                variant="subtitle2"
+                sx={{
+                    alignSelf: isAIMessage ? "flex-start" : "flex-end",
+                    marginTop: "10px",
+                }}
+            >
+                {senderName}
+            </Typography>
+        </Box>
     );
 };
