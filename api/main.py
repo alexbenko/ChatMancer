661488f19d0421c06825ace4dcd42948e94fc124
Alex Benko <benkoalex521@gmail.com>
2024-01-31 19:42:02 -0800

    Add file upload functionality and PDF retrieval for q and a

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile, File, Form
+from typing import Optional
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from dotenv import load_dotenv
@@ -7,12 +8,10 @@ import os
 from init_askgpt import init_askGpt
 from lib.generate_image import generate_image_from_dalle
 from lib.chatbot import chat_message_history_to_dict
+from lib.retriever import get_pdf_retriever, run_document_q_and_a
 
 class ChatIn(BaseModel):
     question: str
-class ImageDescriptionIn(BaseModel):
-    description: str
-
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
@@ -23,22 +22,49 @@ assert OPENAI_API_KEY is not None
 
 ENVIORNMENT = os.getenv('ENVIORNMENT', 'development')
 print(ENVIORNMENT)
+
 is_production = ENVIORNMENT == 'production'
 model = 'gpt-4' if is_production else 'gpt-3.5-turbo'
+
 conversation = init_askGpt(OPENAI_API_KEY, model=model)
-
-
 app = FastAPI()
+
 @app.get("/chat")
 async def get_chat():
     chat_history = chat_message_history_to_dict(conversation.memory.dict())
+
     return {"response": chat_history}
 
-@app.post("/chat")
-async def post_chat(chat_in: ChatIn):
-    question = chat_in.question
+loaded_file = None
+@app.get("/context_file")
+async def get_context_file():
+    global loaded_file
+    return {"response": loaded_file}
 
-    if question.lower().startswith("/image"):
+@app.post("/clear_context_file")
+async def reset_context_file():
+    global loaded_file
+    loaded_file = None
+    return {"response": "Context file has been reset"}
+
+@app.post("/chat")
+async def post_chat(question: str = Form(...), file: Optional[UploadFile] = File(None)):
+    global loaded_file
+    if question.lower().startswith("/pdf") and loaded_file or (file and file.filename.endswith('.pdf')):
+        print('PDF !!!!!')
+        if file:
+            contents = file.file.read()
+
+            with open(file.filename, 'wb') as f:
+                f.write(contents)
+
+            loaded_file = file.filename
+
+        retriever = get_pdf_retriever(OPENAI_API_KEY, file_path=loaded_file)
+        ai_response = run_document_q_and_a(conversation, retriever, question)
+
+        response = {"text": ai_response}
+    elif question.lower().startswith("/image"):
         image_desc = question.lower().replace("/image", "").strip()
         image_url = generate_image_from_dalle(image_desc)
 
