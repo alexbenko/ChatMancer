b4f10e6f0fc8f9a79aa8d50d2e093fe861048358
Alex Benko <benkoalex521@gmail.com>
2024-02-12 11:21:27 -0800

    fixed issue where the chatbot conversation wouuld be loaded multiple times before server startup

--- api/main.py
+++ api/main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from init_chatbot import init_chatbot
 from lib.generate_image import generate_image_from_dalle
@@ -21,7 +22,6 @@ assert PASSWORD is not None
 assert OPENAI_API_KEY is not None
 
 ENVIORNMENT = os.getenv('ENVIORNMENT', 'development')
-print(ENVIORNMENT)
 
 is_production = ENVIORNMENT == 'production'
 model = 'gpt-4' if is_production else 'gpt-3.5-turbo'
@@ -31,9 +31,21 @@ origins = [
     "http://localhost:5173/"
 ]
 
-conversation = init_chatbot(OPENAI_API_KEY, model=model)
+conversation = None
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global conversation
+    conversation = init_chatbot(OPENAI_API_KEY, model=model)
+    print("Chatbot initialized")
+
+    yield
+
+    # Clean up
+    conversation = None
+    print("Chatbot destroyed")
+
+app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -41,8 +53,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 @app.get("/chat")
 async def get_chat():
+    global conversation
     chat_history = chat_message_history_to_dict(conversation.memory.dict())
 
     return {"response": chat_history}
@@ -67,7 +82,7 @@ async def reset_context_file():
 
 @app.post("/chat")
 async def post_chat(question: str = Form(...), file: Optional[UploadFile] = File(None)):
-    global loaded_file
+    global loaded_file, conversation
     if question.lower().startswith("/pdf") and loaded_file or (file and file.filename.endswith('.pdf')):
         print('PDF !!!!!')
         if file and not loaded_file:
@@ -113,5 +128,6 @@ if is_production:
 
 if __name__ == '__main__':
     import uvicorn
+    print(f'Starting server in {ENVIORNMENT} mode.')
     to_run = app if is_production else "main:app"
     uvicorn.run(to_run, host="0.0.0.0", port=8000, log_level="debug", reload= not is_production)
